e9ee28ccd67ca8304e8b37ae2c6d42356a31dc26
York Xiang <bombless@126.com>
2016-08-07 17:38:41 +0800

    Fix square-spin.js

--- square-spin.js
+++ square-spin.js
@@ -18,16 +18,15 @@
 	}
 
 	var getVertexList = (function() {
-		var vertices_tpl = [0, Math.PI / 2, Math.PI, Math.PI, 3 * Math.PI / 2, 0];
+		var vertices_tpl = [Math.PI / 4, 3 * Math.PI / 4, 5 * Math.PI / 4, 5 * Math.PI / 4, 7 * Math.PI / 4, Math.PI / 4];
 		var NUM = 4;
-		var first_time = true;
 		return function() {
 			var i, j;
 			var output = [];
 			var diff = [];
 			var gray;
 			for (i = 0; i < NUM; ++i) {
-				diff.push((+new Date/1000 + Math.PI/NUM*i)%(2*Math.PI));
+				diff.push((+new Date/1000 + Math.PI*2/NUM*i)%(2*Math.PI));
 			}
 			for (i = 0; i < NUM; ++i) {
 				for (j = 0; j < vertices_tpl.length; ++j) {
@@ -35,12 +34,8 @@
 					output.push(diff[i]);
 					gray = 1/NUM*i*.7+.2;
 					output.push(gray);
-					if (first_time) {
-						console.log(gray);
-					}
 				}
 			}
-			first_time = false;
 			return output;
 		};
 	})();
@@ -59,13 +54,26 @@
 	
 	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(list), gl.DYNAMIC_DRAW);
 
-	var vs = 'attribute float pos;attribute float angle;attribute float gray;varying float factor;' + 'void main() { gl_Position = vec4(sin(pos) * cos(angle), cos(pos), 0, 1); factor = gray; }';
-	var fs = 'precision mediump float;uniform vec3 color;varying float factor;' + 'void main() { gl_FragColor = vec4(factor, factor, factor, 1); }';
+	gl.enable(gl.CULL_FACE);
+	gl.cullFace(gl.FRONT);
+	
+	var vs = 'attribute float pos;attribute float angle;attribute float gray;varying float factor;' +
+	`void main() {
+		float x = sin(pos);
+		float y = cos(pos);
+		float z = 1. / sqrt(2.);
+
+		float new_x = x * cos(angle) - z * sin(angle);
+		float new_y = y;
+		float new_z = x * sin(angle) + z * cos(angle);
+		gl_Position = vec4(new_x * .7, new_y * .7, new_z * .7, 1.);
+		factor = gray;
+	}`;
+	var fs = 'precision mediump float;varying float factor;' + 'void main() { gl_FragColor = vec4(factor, factor, factor, 1); }';
 
 	var program = createProgram(vs, fs);
 	gl.useProgram(program);
 	
-	var color = gl.getUniformLocation(program, 'color');
 	var pos = gl.getAttribLocation(program, 'pos');
 	var angle = gl.getAttribLocation(program, 'angle');
 	var gray = gl.getAttribLocation(program, 'gray');
@@ -75,7 +83,6 @@
 	gl.vertexAttribPointer(angle, 1, gl.FLOAT, false, 3 * 4, 4);
 	gl.enableVertexAttribArray(gray);
 	gl.vertexAttribPointer(gray, 1, gl.FLOAT, false, 3 * 4, 8);
-	gl.uniform3f(color, 1, 0, 0);
 	
 	+function step() {
 		var list = getVertexList();
