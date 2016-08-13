5c66f7a9dc3952e21cf5fcb62488d014f62890fb
York Xiang <bombless@126.com>
2016-08-13 02:41:54 +0000

    Projection!

--- square.js
+++ square.js
@@ -28,24 +28,51 @@
 	var buf = gl.createBuffer();
 	gl.bindBuffer(gl.ARRAY_BUFFER, buf);
 
-	var vertices = [0, Math.PI / 2, Math.PI, Math.PI, 3 * Math.PI / 2, 0];
+	var vertices = [Math.PI / 4, 3 * Math.PI / 4, 5 * Math.PI / 4, 5 * Math.PI / 4, 7 * Math.PI / 4, Math.PI / 4];
+	console.log(vertices);
 	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.DYNAMIC_DRAW);
 
-	var vs = 'attribute float pos;varying float vPos;' + 'void main() { gl_Position = vec4(sin(pos), cos(pos), 0, 1); vPos = pos; }';
-	var fs = 'precision mediump float;varying float vPos;' + 'void main() { gl_FragColor = vec4(abs(mix(sin(vPos), cos(vPos), abs(sin(vPos)))), .8, abs(mix(cos(vPos), sin(vPos), abs(sin(vPos)))), 1); }';
+	var vs = 'attribute float pos;uniform float rotate;varying float vPos;' +
+	`void main() {
+		float x = sin(pos);
+		float y = cos(pos);
+		float z = 0.;
+		float new_x = x;
+		float new_y = y * cos(rotate) - z * sin(rotate);
+		float new_z = y * sin(rotate) + z * cos(rotate);
+		float p_z = new_z * .5 + 1.;
+		gl_Position = vec4(new_x / p_z, new_y / p_z, 0, 1.);
+		vPos = pos;
+	}`;
+	var fs = 'precision mediump float;varying float vPos;' +
+	`void main() {
+		float r;
+		float g;
+		float b;
+		if (vPos < 2.) {
+			r = 1.; g = .0; b = .0;
+		} else if (vPos < 3.) {
+			r = .0; g = 1.; b = .0;
+		} else if (vPos < 5.) {
+			r = .0; g = .0; b = 1.;
+		} else {
+			r = abs(mix(sin(vPos), cos(vPos), abs(sin(vPos))));
+			g = .8;
+			b = abs(mix(cos(vPos), sin(vPos), abs(sin(vPos))));
+		}
+		gl_FragColor = vec4(r, g, b, 1.);
+	}`;
 
 	var program = createProgram(vs, fs);
 	gl.useProgram(program);
 
 	var pos = gl.getAttribLocation(program, 'pos');
+	var rotate = gl.getUniformLocation(program, 'rotate');
 	gl.enableVertexAttribArray(pos);
 	gl.vertexAttribPointer(pos, 1, gl.FLOAT, false, 0, 0);
-	gl.drawArrays(gl.TRIANGLES, 0, 3);
 
 	+function step() {
-		var diff = +new Date/1000%(2*Math.PI);
-		vertices = [diff, Math.PI / 2 + diff, Math.PI + diff, Math.PI + diff, 3 * Math.PI / 2 + diff, diff];
-		gl.bufferSubData(gl.ARRAY_BUFFER, 0, new Float32Array(vertices), gl.DYNAMIC_DRAW);
+		gl.uniform1f(rotate, new Date/1000%(2*Math.PI));
 		gl.drawArrays(gl.TRIANGLES, 0, 6);
 		requestAnimationFrame(step);
 	}()
