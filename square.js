ece3ca903ee2b97009cb19fdf5d66b62c584e728
York Xiang <bombless@126.com>
2016-08-13 07:53:57 +0000

    Load texture to square.js

--- square.js
+++ square.js
@@ -3,6 +3,10 @@
 		var shader = gl.createShader(type);
 		gl.shaderSource(shader, str);
 		gl.compileShader(shader);
+		var success = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
+ 		if (!success) {
+    	throw "could not compile shader:" + gl.getShaderInfoLog(shader);
+  	}
 		return shader;
 	}
 
@@ -36,14 +40,30 @@
 			panel.style.backgroundColor = 'rgb(' + pixels[0] + ',' + pixels[1] + ',' + pixels[2] + ')';
 		}
 	});
-
+	var texture;
+	+function() {
+		var t_ = gl.createTexture();
+		var i_ = new Image();
+		i_.onload = function() {
+			gl.bindTexture(gl.TEXTURE_2D, t_);
+			gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, i_);
+			gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+			gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR_MIPMAP_NEAREST);
+			gl.generateMipmap(gl.TEXTURE_2D);
+			gl.bindTexture(gl.TEXTURE_2D, null);
+			texture = t_;
+			gl.activeTexture(gl.TEXTURE0);
+			gl.bindTexture(gl.TEXTURE_2D, t_);
+		};
+		i_.src = 'texture.png';
+	}();
 	var buf = gl.createBuffer();
 	gl.bindBuffer(gl.ARRAY_BUFFER, buf);
 
 	var vertices = [Math.PI / 4, 3 * Math.PI / 4, 5 * Math.PI / 4, 5 * Math.PI / 4, 7 * Math.PI / 4, Math.PI / 4];
 	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.DYNAMIC_DRAW);
 
-	var vs = 'attribute float pos;uniform float rotate;varying float vPos;' +
+	var vs = 'attribute float pos;uniform float rotate;varying float vPos;varying vec2 texCoord;' +
 	`void main() {
 		float x = sin(pos);
 		float y = cos(pos);
@@ -52,10 +72,11 @@
 		float new_y = y * cos(rotate) - z * sin(rotate);
 		float new_z = y * sin(rotate) + z * cos(rotate);
 		float p_z = new_z * .5 + 1.;
-		gl_Position = vec4(new_x / p_z, new_y / p_z, 0, 1.);
+		texCoord = vec2(.5 + .5 * x, .5 + .5 * y);
+		gl_Position = vec4(new_x, new_y, 0, p_z);
 		vPos = pos;
 	}`;
-	var fs = 'precision mediump float;varying float vPos;' +
+	var fs = 'precision mediump float;varying float vPos;varying highp vec2 texCoord;uniform sampler2D sampler;' +
 	`void main() {
 		float r;
 		float g;
@@ -71,7 +92,7 @@
 			g = .8;
 			b = abs(mix(cos(vPos), sin(vPos), abs(sin(vPos))));
 		}
-		gl_FragColor = vec4(r, g, b, 1.);
+		gl_FragColor = texture2D(sampler, texCoord);
 	}`;
 
 	var program = createProgram(vs, fs);
@@ -82,11 +103,13 @@
 	gl.enableVertexAttribArray(pos);
 	gl.vertexAttribPointer(pos, 1, gl.FLOAT, false, 0, 0);
 	gl.clearColor(1, 1, 1, 1);
+	gl.uniform1i(gl.getUniformLocation(program, 'sampler'), 0);
 
 	+function step() {
+		requestAnimationFrame(step);
+		if (!texture) return;
 		gl.clear(gl.COLOR_BUFFER_BIT);
 		gl.uniform1f(rotate, new Date/1000%(2*Math.PI));
 		gl.drawArrays(gl.TRIANGLES, 0, 6);
-		requestAnimationFrame(step);
 	}()
 }()
